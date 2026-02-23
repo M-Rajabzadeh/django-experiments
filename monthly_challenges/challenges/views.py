76a43d398289fa69c0d3270d24c452c8bc4ae1b3
Mohammad Rajabzadeh <m.rajabzadeh.works@gmail.com>
2026-02-23 12:51:10 +0330

    refactor(challenges): implement dynamic monthly challenge routing and remove hardcoded structure

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-def index(request):
-    return HttpResponse("This Works!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Learn Django at least 20minutes every day" 
+    elif month == "february":
+        challenge_text = "Study for 3 hours python"
+    elif month == "march":
+        challenge_text = "Walk for at least 30 minutes every day"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
